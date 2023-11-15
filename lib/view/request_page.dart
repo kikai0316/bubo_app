9f5151e220d1e9818785186e4103f1e5a26d30c5
daisuke0316 <dshengtian6@gmail.com>
2023-11-15 21:34:49 +0900

    k

--- lib/view/request_page.dart
+++ lib/view/request_page.dart
@@ -19,152 +19,154 @@ class RequestNotificationsPage extends HookConsumerWidget {
     return Scaffold(
       backgroundColor: blackColor,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Expanded(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(top: safeAreaHeight * 0.1),
-                    child: nText(
-                      "通知を許可しますか？",
-                      color: Colors.white,
-                      fontSize: safeAreaWidth / 15,
-                      bold: 500,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: safeAreaHeight * 0.1),
+                      child: nText(
+                        "通知を許可しますか？",
+                        color: Colors.white,
+                        fontSize: safeAreaWidth / 15,
+                        bold: 500,
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                      top: safeAreaHeight * 0.05,
-                      bottom: safeAreaHeight * 0.11,
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: safeAreaHeight * 0.05,
+                        bottom: safeAreaHeight * 0.11,
+                      ),
+                      child: nText(
+                        "アプリからの通知を受け取るためには、\n通知を許可してください。",
+                        color: Colors.grey,
+                        fontSize: safeAreaWidth / 25,
+                        bold: 500,
+                      ),
                     ),
-                    child: nText(
-                      "アプリからの通知を受け取るためには、\n通知を許可してください。",
-                      color: Colors.grey,
-                      fontSize: safeAreaWidth / 25,
-                      bold: 500,
-                    ),
-                  ),
-                  SizedBox(
-                    height: safeAreaHeight * 0.25,
-                    width: safeAreaWidth * 0.8,
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Container(
-                          height: safeAreaHeight * 0.21,
-                          width: safeAreaWidth * 0.8,
-                          decoration: BoxDecoration(
-                            color: whiteColor,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Column(
-                            children: [
-                              Container(
-                                alignment: Alignment.center,
-                                height: safeAreaHeight * 0.14,
-                                width: double.infinity,
-                                decoration: const BoxDecoration(
-                                  border: Border(
-                                    bottom: BorderSide(
-                                      color: Colors.grey,
-                                      width: 0.5,
+                    SizedBox(
+                      height: safeAreaHeight * 0.25,
+                      width: safeAreaWidth * 0.8,
+                      child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Container(
+                            height: safeAreaHeight * 0.21,
+                            width: safeAreaWidth * 0.8,
+                            decoration: BoxDecoration(
+                              color: whiteColor,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            child: Column(
+                              children: [
+                                Container(
+                                  alignment: Alignment.center,
+                                  height: safeAreaHeight * 0.14,
+                                  width: double.infinity,
+                                  decoration: const BoxDecoration(
+                                    border: Border(
+                                      bottom: BorderSide(
+                                        color: Colors.grey,
+                                        width: 0.5,
+                                      ),
+                                    ),
+                                  ),
+                                  child: nText(
+                                    "BUBUは通知を送信します。\nよろしいですか？",
+                                    color: blackColor.withOpacity(0.9),
+                                    fontSize: safeAreaWidth / 23,
+                                    bold: 700,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: SizedBox(
+                                    width: double.infinity,
+                                    child: Row(
+                                      children: [
+                                        for (int i = 0; i < 2; i++) ...{
+                                          Expanded(
+                                            child: Container(
+                                              alignment: Alignment.center,
+                                              height: double.infinity,
+                                              child: nText(
+                                                i == 0 ? "許可しない" : "許可",
+                                                color:
+                                                    blackColor.withOpacity(0.4),
+                                                fontSize: safeAreaWidth / 26,
+                                                bold: 700,
+                                              ),
+                                            ),
+                                          ),
+                                          if (i == 0)
+                                            Container(
+                                              width: 0.5,
+                                              height: double.infinity,
+                                              color: Colors.grey,
+                                            ),
+                                        },
+                                      ],
                                     ),
                                   ),
                                 ),
+                              ],
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Padding(
+                              padding: EdgeInsets.only(
+                                right: safeAreaWidth * 0.08,
+                              ),
+                              child: Container(
+                                alignment: Alignment.center,
+                                width: safeAreaHeight * 0.11,
+                                height: safeAreaHeight * 0.11,
+                                decoration: const BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: whiteColor,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: blueColor2,
+                                      blurRadius: 20,
+                                      spreadRadius: 10.0,
+                                    ),
+                                  ],
+                                ),
                                 child: nText(
-                                  "BUBUは通知を送信します。\nよろしいですか？",
-                                  color: blackColor.withOpacity(0.9),
-                                  fontSize: safeAreaWidth / 23,
+                                  "許可",
+                                  color: blueColor2,
+                                  fontSize: safeAreaWidth / 19,
                                   bold: 700,
                                 ),
                               ),
-                              Expanded(
-                                child: SizedBox(
-                                  width: double.infinity,
-                                  child: Row(
-                                    children: [
-                                      for (int i = 0; i < 2; i++) ...{
-                                        Expanded(
-                                          child: Container(
-                                            alignment: Alignment.center,
-                                            height: double.infinity,
-                                            child: nText(
-                                              i == 0 ? "許可しない" : "許可",
-                                              color:
-                                                  blackColor.withOpacity(0.4),
-                                              fontSize: safeAreaWidth / 26,
-                                              bold: 700,
-                                            ),
-                                          ),
-                                        ),
-                                        if (i == 0)
-                                          Container(
-                                            width: 0.5,
-                                            height: double.infinity,
-                                            color: Colors.grey,
-                                          ),
-                                      },
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Align(
-                          alignment: Alignment.bottomRight,
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                              right: safeAreaWidth * 0.08,
-                            ),
-                            child: Container(
-                              alignment: Alignment.center,
-                              width: safeAreaHeight * 0.11,
-                              height: safeAreaHeight * 0.11,
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: whiteColor,
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: blueColor2,
-                                    blurRadius: 20,
-                                    spreadRadius: 10.0,
-                                  ),
-                                ],
-                              ),
-                              child: nText(
-                                "許可",
-                                color: blueColor2,
-                                fontSize: safeAreaWidth / 19,
-                                bold: 700,
-                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: safeAreaHeight * 0.25,
-                bottom: safeAreaHeight * 0.02,
+              Padding(
+                padding: EdgeInsets.only(
+                  top: safeAreaHeight * 0.25,
+                  bottom: safeAreaHeight * 0.02,
+                ),
+                child: shadowButton(
+                  context,
+                  text: "通知をONにする",
+                  onTap: () async {
+                    await NotificationClass().requestPermissions();
+                    // ignore: use_build_context_synchronously
+                    screenTransitionNormal(context, const UserApp(initPage: 0));
+                  },
+                ),
               ),
-              child: shadowButton(
-                context,
-                text: "通知をONにする",
-                onTap: () async {
-                  await NotificationClass().requestPermissions();
-                  // ignore: use_build_context_synchronously
-                  screenTransitionNormal(context, const UserApp(initPage: 0));
-                },
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
